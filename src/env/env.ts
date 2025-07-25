51a4b87fb3ecfd1947d41c5ba7f3923a25c9072a
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-07-25 15:40:00 -0300

    feat: add DATABASE_URL to env.ts validation

--- src/env/env.ts
+++ src/env/env.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
+  DATABASE_URL: z.url().startsWith('postgresql://'),
 });
 
 // Generate TypeScript type from the schema
