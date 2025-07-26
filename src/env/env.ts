f43afac247572cfb4d9aebbf9a9110d22ae92163
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-07-26 13:54:37 -0300

    chore: add Shopify and Ngrok Host environment variables to env file and validation

--- src/env/env.ts
+++ src/env/env.ts
@@ -3,6 +3,10 @@ import { z } from "zod";
 export const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
   DATABASE_URL: z.url().startsWith('postgresql://'),
+  SHOPIFY_API_KEY: z.string(),
+  SHOPIFY_API_SECRET: z.string(),
+  SHOPIFY_SCOPES: z.string(),
+  SHOPIFY_REDIRECT_URI: z.url(),
 });
 
 // Generate TypeScript type from the schema
