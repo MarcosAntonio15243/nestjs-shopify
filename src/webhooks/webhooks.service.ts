3e56dc41b14d5caba16be2f1201f6790dab7cf01
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-07-30 21:41:02 -0300

    feat(webhook): persist customer information when creating an order

--- src/webhooks/webhooks.service.ts
+++ src/webhooks/webhooks.service.ts
@@ -1,9 +1,10 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import * as crypto from "crypto";
 import { DRIZZLE } from 'src/drizzle/drizzle.module';
 import { DrizzleDB } from 'src/drizzle/types/types';
 import { OrderDTO } from './dto/order.dto';
 import { schema } from 'src/drizzle/schema';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
 export class WebhooksService {
@@ -22,7 +23,35 @@ export class WebhooksService {
   }
 
   async saveOrder(order: OrderDTO) {
-    await this.db.insert(schema.orders).values({
+    let customer_id: string | null = null;
+    
+    if (order.customer) {
+      let [customer] = await this.db.select({
+          id: schema.customers.id
+        })
+        .from(schema.customers)
+        .where(eq(schema.customers.idShopify, order.customer.id));
+      
+      if (!customer) {
+        const result1 = await this.db.insert(schema.customers).values({
+          idShopify: order.customer.id,
+          email: order.customer.email,
+          firstName: order.customer.first_name,
+          lastName: order.customer.last_name,
+          phone: order.customer.phone
+        }).returning({ id: schema.customers.id });
+
+        customer = result1[0];
+
+        if (!customer) {
+          throw new BadRequestException('Failed to persist customer data');
+        }
+      }
+
+      customer_id = customer.id;
+    }
+    
+    const result2 = await this.db.insert(schema.orders).values({
       idShopify: order.id,
       financialStatus: order.financial_status,
       createdAt: new Date(order.created_at),
@@ -35,7 +64,13 @@ export class WebhooksService {
       gateway: order.gateway,
       note: order.note,
       tags: order.tags,
-    });
+      customerId: customer_id
+    }).returning();
+
+    const insertedOrder = result2[0];
+    if (!insertedOrder) {
+      throw new BadRequestException('Failed to persist order data');
+    }
   }
 
 }
