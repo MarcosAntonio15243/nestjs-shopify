14ab6a0f287fe23ca238db8b6752ff0ce22dcfb8
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-08-01 01:15:56 -0300

    chore: move drizzle config to src/drizzle and update related scripts

--- src/drizzle/drizzle.config.ts
+++ src/drizzle/drizzle.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   dialect: 'postgresql',
   casing: 'camelCase',
-  schema: './src/drizzle/schema/**.ts',
+  schema: './src/drizzle/schema/**/*.ts',
   out: './src/drizzle/migrations',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
