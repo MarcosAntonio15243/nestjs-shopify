e1848af34bf54bde828d4d36a0d96820556ed88e
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-08-01 23:39:31 -0300

    refactor: remove unused attributes and imports

--- src/webhooks/webhooks.controller.ts
+++ src/webhooks/webhooks.controller.ts
@@ -1,20 +1,17 @@
 import { Controller, Headers, HttpStatus, Post, Req, Res, UseInterceptors } from '@nestjs/common';
 import { WebhooksService } from './webhooks.service';
 import { Request, Response } from 'express';
-import { ConfigService } from '@nestjs/config';
 import { ShopifyHmacInterceptor } from '../common/interceptors/shopify-hmac.interceptor';
 
 @Controller('webhooks')
 export class WebhooksController {
   constructor(
     private readonly webhooksService: WebhooksService,
-    private readonly configService: ConfigService
   ) {}
 
   @Post("orders/create")
   @UseInterceptors(ShopifyHmacInterceptor)
   orderCreated(
-    @Headers('x-shopify-hmac-sha256') hmac: string,
     @Req() req: Request,
     @Res() res: Response
   ) {
--- src/webhooks/webhooks.module.ts
+++ src/webhooks/webhooks.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WebhooksController } from './webhooks.controller';
 import { WebhooksService } from './webhooks.service';
-import { ConfigModule } from '@nestjs/config';
 import { DrizzleModule } from '../drizzle/drizzle.module';
 
 @Module({
-  imports: [ConfigModule, DrizzleModule],
+  imports: [DrizzleModule],
   controllers: [WebhooksController],
   providers: [WebhooksService]
 })
